edcc2ebb25e962a2d233f42436a6721fb46d4740
mohsohail <sohail.momin1@gmail.com>
2020-12-28 23:09:48 +0530

    update: creates popup ui (intermediate)

---
+++ ui/collectmark_script.js
@@ -0,0 +1,36 @@
+const bookmarkFolderMap = {
+  bookmarksBar: "1",
+  otherBookmarksBar: "2",
+  mobileBookmarksBar: "3",
+};
+
+const button = document.querySelector("button");
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  chrome.tabs.query({}, function (tabs) {
+    let urlsToBookmark = [];
+    if (tabs.length) {
+      tabs.forEach(function (tab) {
+        const { title, url } = tab;
+        urlsToBookmark.push({
+          title,
+          url,
+        });
+      });
+    }
+    if (urlsToBookmark.length !== 0) {
+      bookmarkUrls(urlsToBookmark);
+    }
+  });
+}
+
+function bookmarkUrls(urls) {
+  urls.forEach(function (urlData) {
+    chrome.bookmarks.create({
+      parentId: bookmarkFolderMap["bookmarksBar"],
+      title: urlData.title,
+      url: urlData.url,
+    });
+  });
+}
